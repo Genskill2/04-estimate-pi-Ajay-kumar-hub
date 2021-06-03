4efebd226c58135899d0696dd367c216688b9e4a
Ajay-kumar-hub <85028376+Ajay-kumar-hub@users.noreply.github.com>
2021-06-03 20:52:20 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -5,11 +5,26 @@
 
 float wallis_pi(int);
 
+ float wallis_pi(int n)
+ {
+   float k=1.0;
+      int i;
+       for(i=1;i<=n;i++)
+       {
+         k *=(float)(4*i*i)/4*i*i-1);
+       }
+   
+   
+   return (k*2);
+ }
+
 int main(void) {
   float pi;
   for (int i=0; i<5; i++) {
     pi = wallis_pi(i);
+    //printf("wallis called %f %f\n",M_PI,pi);
     if (!(fabs(pi - M_PI) > 0.15)) {
+      //printf("wallis g %f\n",pi) ;
       printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
       abort();
     }
@@ -23,15 +38,4 @@ int main(void) {
     }
   }
 }
-
-float wallis_pi(int n)
-{ 
-  float ret=1.0;
-  float base;
-   for(int i=1;i<=n;i++)
-   {  base=(float)(4*i*i)/((4*i*i)-1);
-      ret=ret*base;
-   } 
-  ret=ret*2;
-   return ret;
-}   
+   
