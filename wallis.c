7b9e200287a7f521d48f84ca3cb31cbff3f67cae
Ajay-kumar-hub <85028376+Ajay-kumar-hub@users.noreply.github.com>
2021-06-03 07:02:37 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -4,27 +4,14 @@
 #include <math.h>
 
 float wallis_pi(int n)
-{
 	
-	float temp,temp1,temp2,pei;
-	for(int i=0;i<n;i++)
-{
-	temp=4*(pow(1,2));
-	temp1=temp-1;
-	temp2=temp/temp1;
-	
-}
-pei=2*temp2;
-return(pei);
-}
-
-int main(void) {
+ int main(void) {
   float pi;
   for (int i=0; i<5; i++) {
     pi = wallis_pi(i);
     if (!(fabs(pi - M_PI) > 0.15)) {
       printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
-      /*abort();*/
+      abort();
     }
   }
 
@@ -32,7 +19,7 @@ int main(void) {
     pi = wallis_pi(i);
     if (!(fabs(pi - M_PI) < 0.01)) {
       printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi);
-      /*abort();*/
+      abort();
     }
   }
 }
