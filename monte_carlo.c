0e33e8b57d2415e1cc4806d6b888d467390b2c54
Ajay-kumar-hub <85028376+Ajay-kumar-hub@users.noreply.github.com>
2021-06-03 11:17:56 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -48,10 +48,12 @@ float mc_oi(int n)
       z=fradom();
       t=(y*y)+(z*z);
       if(t<=1)
-     {  within_circle++;
+     {  
+        within_circle++;
      }
      else 
-     {  oustside_circle++;
+     {  
+       oustside_circle++;
      }
   }
   total_points= within_circle+ outside_circle;
